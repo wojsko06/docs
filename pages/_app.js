5418030adf126550c0ec1e615296b74f295a9c7e
wojsko06 <w.skowron@tisagroup.ch>
2024-10-25 17:48:41 +0200

    Initialize fix-env

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import {
+  createGlobalStyle,
+  ThemeProvider,
+  Preflight,
+  ColorModeProvider,
+  th,
+} from "@xstyled/styled-components";
+
+import { theme } from "../config/theme";
+const GlobalStyle = createGlobalStyle`
+  ${th("global")}
+`;
+import TinaProvider from "../.tina/components/TinaDynamicProvider";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider>
+        <Preflight />
+        <GlobalStyle />
+        <TinaProvider>
+          <Component {...pageProps} />
+        </TinaProvider>
+      </ColorModeProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
